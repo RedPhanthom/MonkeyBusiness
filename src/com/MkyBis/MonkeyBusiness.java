c1dc30d1def24665102f7b3c938c2535d4dcdfeb
RedPhanthom <Phanthom@HuskInt>
2017-01-14 23:05:54 -0500

    Finished up assingment to my complete extend of what I know.
    • Average amount of food eaten daily by the whole family of monkeys.
    • Average amount of food eaten weekly by each monkey.
    • The least daily amount of food eaten during the week by any one
    monkey.
    • The greatest daily amount of food eaten during the week by any one
    monkey.
    
    I tried my best to what this assingment asked me to do. Dont know if
    even half the stuff works correctly.

--- src/com/MkyBis/MonkeyBusiness.java
+++ src/com/MkyBis/MonkeyBusiness.java
@@ -1,8 +1,69 @@
+/*--------------------------------------------------------------------
+Name: Jaime Palencia
+Student ID: U94665425
+COP 2805C - Java Programming 2
+Spring /2017
+Assignment # 0
+
+Plagiarism Statement
+
+I certify that this assignment is my own work and that I
+have not copied in part or whole or otherwise plagiarized 
+the work of other students and/or persons.
+--------------------------------------------------------------------*/
 package com.MkyBis;
 
+import java.util.Scanner;
+
 public class MonkeyBusiness {
-	final int numOfMnks = 4;
-	final int numOfDays = 7;
-	
-	int[][] monkeyArray = new int[numOfMnks][numOfDays]; 
+	public static void main(String[] args) {
+		// Variables
+		final int numOfMnks = 4;
+		final int numOfDays = 7;
+		int avgDaily = 0, avgWeekly = 0, total = 0, least = 0, most = 0;
+
+		// X * X 2D Array
+		int[][] monkeyArray = new int[numOfMnks][numOfDays];
+
+		// Keyboard Scanner
+		Scanner keyIn = new Scanner(System.in);
+
+		System.out.println("Enter How Much Food Is Eaten For Each Monkey.");
+
+		try {
+			for (int row = 0; row < numOfMnks; row++) {
+				for (int col = 0; col < numOfDays; col++) {
+					do {
+						System.out.printf("Monkey %d, Day %d: ", (row + 1), (col + 1));
+						monkeyArray[row][col] = keyIn.nextInt();
+						if(monkeyArray[row][col] <= -1 || monkeyArray[row][col] >= 50){
+							System.out.println("Number can't be negative, Or greater than 50.");
+							monkeyArray[row][col] = keyIn.nextInt();
+						}
+
+					} while (monkeyArray[row][col] <= -1 || monkeyArray[row][col] >= 50);
+				}
+			}
+
+			for (int row = 0; row < numOfMnks; row++) {
+				for (int col = 0; col < numOfDays; col++) {
+					if(monkeyArray[row][col] < least){
+						least = monkeyArray[row][col];
+					}
+					if(monkeyArray[row][col] > most){
+						most = monkeyArray[row][col];
+					}
+					total += monkeyArray[row][col];
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Total: " + total + " Pounds");
+		System.out.println("Weekly Average: " + avgWeekly/21.0 + " Pounds");
+		System.out.println("Least Amount: " + least + " Pounds");
+		System.out.println("Most Amount: " + most + " Pounds");
+	}
+
 }
